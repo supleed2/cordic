550827217c135c2236c65c4f5444e0e7f5d710cd
Aadi Desai <21363892+supleed2@users.noreply.github.com>
2023-05-30 14:15:44 +0100

    Output error from expected correctly, - means low

--- test_saw2sin.py
+++ test_saw2sin.py
@@ -11,11 +11,10 @@ async def test_new_cordic(dut):
         dut.i_saw.value = cycle
         await Timer(1, units='ps')
         e_sin = 32768 * (sin((cycle * pi) / (2**15)) + 1)
-        error = e_sin - dut.o_sin.value
+        error = float(dut.o_sin.value) - e_sin
         if abs(error) > 2:
-            dut._log.info("cycle %5d, rev %d, inv %d, expected %5d, got %5d, error %d"
+            dut._log.info("cycle %5d, rev %d, inv %d, expected %5d, got %5d, error %+.6f"
                 % (cycle, (cycle & 0x4000) != 0, (cycle & 0x8000) != 0, e_sin, dut.o_sin.value, error))
         diff += abs(error)
 
     dut._log.info("Testbench finished, average error %f" % (diff / 65536))
-
